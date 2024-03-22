ef1732ab8187ceddc226e219d4546cfecb4e3173
PraneethKumarReddy <praneeth.reddy@clear.in>
2024-03-22 17:45:47 +0530

    workflow

--- .github/actions/clickhouse/clickhouse.sh
+++ .github/actions/clickhouse/clickhouse.sh
@@ -4,4 +4,5 @@ docker run -d -e CLICKHOUSE_DB="$CLICKHOUSE_DB" \
 -e CLICKHOUSE_USER="$CLICKHOUSE_USER" \
 -e CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT=1 \
 -e CLICKHOUSE_PASSWORD="$CLICKHOUSE_PASSWORD" \
--p 9000:9000/tcp -p 8123:8123/http clickhouse/clickhouse-server:"$INPUT_VERSION-alpine"
+-p 9000:9000 -p 8123:8123 --name clickhouse \
+clickhouse/clickhouse-server:"$INPUT_VERSION-alpine"
